f5145e4aed373a4f5bfe9a761eda4842c144c12f
Kevin Deldycke <kevin@deldycke.com>
2005-01-23 23:45:00 +0100

    Add a copyright header. Add some documentation and comments. Ignore an entire group from saving if all of its items are not present on the system. Add an option to ignore the username in path. Add a silent option.

--- confsaver.py
+++ confsaver.py
@@ -1,15 +1,39 @@
 #!/usr/bin/python
 
+##############################################################################
+#
+# Copyright (C) 2004-2005 Kevin Deldycke <kevin.deldycke@free.fr>
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+##############################################################################
+
 """
-Last update : 2004 dec 05
+Last update : 2005 jan 23
 
 Improvements :
+
+  * Use a --force -f option to automatically erase the user files
+
+  * Make a --verbose -v option to reduce the printing
 
   * tar c dir/ | gzip | gpg -c | ssh user@remote 'dd of=dir.tar.gz.gpg'
     make encrypted archive of dir/ on remote machine
 
   * Take care of special encoded char in url like ", ', ?, [space], accents, /, \, and so on
-    
+
   * Take care of owner/group/rights
 
   * Save the config_files list as a serialized object in a file with a special name
@@ -19,7 +43,7 @@ Improvements :
 
 
 
-import getopt, sys, os, tempfile, random, datetime
+import getopt, sys, os, tempfile, datetime
 from commands import getstatusoutput
 from os import system
 
@@ -29,54 +53,93 @@ home_dir = os.path.abspath(os.getenv('HOME'))
 temp_dir = home_dir
 user_name = os.getenv('USER')
 
+# Set default values of user parameters
+complete_group_required = False
+silent = False
 
-# 'd' indicate an entire directory to save, 'f' just a file to save
-config_files = { 'Kopete': [('d', '.kde/share/apps/kopete')
-                           ,('f', '.kde/share/config/kopeterc')
-                           ]
-               , 'Kmail' : [('d', '.kde/share/apps/kmail/')
-                           ,('f', '.kde/share/config/kmailrc')
-                           ]
-               , 'Mail boxes'  : [('d', '.Mail/')
-                           ]
-               , 'Konqueror bookmark': [('f', '.kde/share/apps/konqueror/bookmarks.xml')
-                                 ]
+# The structure of the list of elements to save in the archive look like this :
+# congig_file = { 'First group name' : [('item type', 'item path')
+#                                      ,('item type', 'item path')
+#                                      ,('item type', 'item path')
+#                                      ]
+#               , 'Second group name' : [('item type', 'item path')
+#                                       ,('item type', 'item path')
+#                                       ,('item type', 'item path')
+#                                       ]
+#               }
+# Directories and files path are given as relative, starting from the user home folder
+# Types of items to save for a given group can be of 2 types :
+#     'd' -> indicate an entire directory to save
+#     'f' -> indicate a single file to save
+
+config_files = { 'Kopete'             : [('d', '.kde/share/apps/kopete')
+                                        ,('f', '.kde/share/config/kopeterc')
+                                        ]
+               , 'Kmail'              : [('d', '.kde/share/apps/kmail/')
+                                        ,('f', '.kde/share/config/kmailrc')
+                                        ]
+               , 'Mail boxes'         : [('d', '.Mail/')
+                                        ]
+               , 'Konqueror bookmark' : [('f', '.kde/share/apps/konqueror/bookmarks.xml')
+                                        ]
                }
 
 
 
 def usage():
   print """Usage : confsaver [options]
-Options :
+
+** Saving options
+    -s, --save
+            Save the current desktop configuration to a file.
+    -c, --complete
+            Only save a group if all items exists.
+    --nodate
+            Don't include the current date to the archive name while saving.
+    --nouser
+            Don't include the current user login to the archive name while saving.
+
+** Restoring options
     -r  file, --restore=FILE
             Restore the desktop configuration of the current user from the given file.
             WARNING : restoring will erase existing files without asking.
-    -s, --save
-            Save the current desktop configuration to a file.
-    --nodate
-            Don't include the current date to the archive name while saving.
+
+** General options
+    --silent
+            No informations will be printed on the standard output.
     -h, --help
             Show this screen.
 """
-  return
+
+
+
+def output(string):
+  if not silent:
+    print string
 
 
 
 def create_temp_folder():
-
-  print "* Create a temporary folder..."
+  output("* Create a temporary folder...")
   temp_folder_path = tempfile.mkdtemp(prefix="confsaver-")
-  print "    Created : " + temp_folder_path + "/"
+  temp_folder_path += '/'
+  output("    Created : " + temp_folder_path)
   return temp_folder_path
 
 
 
 def remove_temp_folder(temp_folder_path):
-  return
   # Remove temp files
-  print "* Remove the temporary folder..."
+  output("* Remove the temporary folder...")
   system("rm -rf " + temp_folder_path)
-  print "    Removed : " + temp_folder_path + "/"
+  output("    Removed : " + temp_folder_path)
+
+
+
+def makeFolderReal(folder_path):
+  # This function check that the path exist and create folders if needed
+  if not os.path.exists(folder_path):
+    os.makedirs(folder_path, 0755)
 
 
 
@@ -86,24 +149,40 @@ def save_config(use_date, use_user):
   temp_folder_path = create_temp_folder()
 
   # Save config
-  for item in config_files.keys():
-    print "* Save configuration of " + item + "..."
-    for element in config_files[item]:
-      if element[0] == 'd':
-        path = element[1]
-      else:
-        path = '/'.join(element[1].split('/')[:-1])
-      system("mkdir -p " + temp_folder_path + '/' + path)
-      reduced_path = element[1].split('/')
-      if reduced_path[-1] == '':
-        reduced_path = reduced_path[:-1]
-      reduced_path = '/'.join(reduced_path[:-1])
-      if len(reduced_path) > 0:
-        reduced_path += '/'
-      source_path = home_dir + '/' + element[1]
-      # TODO: manage the case of inexisting source_path
-      system("cp -r " + source_path + ' ' + temp_folder_path + '/' + reduced_path)
-      print "    Saved : " + source_path
+  for group in config_files.keys():
+    output("* Save configuration of " + group + "...")
+
+    # Check the existence of all items if it was asked by the user
+    item_missing = False
+    if complete_group_required:
+      for item in config_files[group]:
+        source_path = home_dir + '/' + item[1]
+        if not os.path.exists(source_path):
+          item_missing = True
+          break
+
+    # Copy the next item or skip the entire group according to user options
+    if item_missing:
+      output("    Skipped " + group + " group because of a missing item ("+ source_path +")")
+    else:
+      for item in config_files[group]:
+        source_path = home_dir + '/' + item[1]
+        if not os.path.exists(source_path):
+          output("    Skipped (item not found) : " + source_path)
+        else:
+          if item[0] == 'd':
+            path = item[1]
+          else:
+            path = '/'.join(item[1].split('/')[:-1])
+          reduced_path = item[1].split('/')
+          if reduced_path[-1] == '':
+            reduced_path = reduced_path[:-1]
+          reduced_path = '/'.join(reduced_path[:-1])
+          if len(reduced_path) > 0:
+            reduced_path += '/'
+          system("mkdir -p " + temp_folder_path + path)
+          system("cp -r " + source_path + ' ' + temp_folder_path + reduced_path)
+          output("    Saved : " + source_path)
 
   # Make an archive
   archive_name = "confsave"
@@ -111,14 +190,13 @@ def save_config(use_date, use_user):
     archive_name += "-" + user_name
   if use_date == True:
     date_element = datetime.date.today().timetuple()
-    # TODO: set month and day to 2-digit format
-    archive_name += "-" + str(date_element[0]) + "_" + str(date_element[1]) + "_" + str(date_element[2])
+    archive_name += "-%04d_%02d_%02d" % (date_element[0], date_element[1], date_element[2])
   archive_name += ".tar.bz2"
   # TODO: make sure the file "archive_name" doesn't exist
-  print "* Create the archive " + archive_name + "..."
+  output("* Create the archive " + archive_name + "...")
   archive_abs_path = os.path.abspath("./" + archive_name)
-  system("tar c -C " + temp_folder_path + " ./ | bzip2 > " + archive_abs_path)
-  print "    Created : " + archive_abs_path
+  system("tar c -C " + temp_folder_path[:-1] + " ./ | bzip2 > " + archive_abs_path)
+  output("    Created : " + archive_abs_path)
 
   # Remove temp files
   remove_temp_folder(temp_folder_path)
@@ -135,30 +213,51 @@ def restore_config(archive):
 
   # Extract files to temp folder
   archive_name = os.path.split(archive)[1]
-  print "* Unpack configuration to temp folder..."
-  system("bzip2 -dc " + archive_abs_path + " | tar x -C " + temp_folder_path + "/")
-  print "    Unpacked : " + archive_name
+  output("* Unpack configuration to temp folder...")
+  system("bzip2 -dc " + archive_abs_path + " | tar x -C " + temp_folder_path)
+  output("    Unpacked : " + archive_name)
 
   # TODO : get here the serialized object and save it to config_files
 
-  # Restoring all the configuration files
-  for item in config_files.keys():
-    print "* Restore current user's files and folders of " + item + " configuration..."
-    for element in config_files[item]:
+  # Restoring all groups
+  for group in config_files.keys():
+    output("* Restore files of " + group + " group...")
 
-      # Erase the every destination file or directory to prevent conflict
-      to_remove = home_dir + '/' + element[1]
-      system("rm -rf " + to_remove)
-      print "    Removed : " + to_remove
+    # Check if the group is contain all suposed items
+    group_is_complete = False
+    items_number = 0
+    for item in config_files[group]:
+      source_path = temp_folder_path + item[1]
+      if os.path.exists(source_path):
+        items_number += 1
 
-      # Copy all configuration file to the home dir
-      # TODO: before copying, make sure that 'intermediate' folders exists
-      relative_path = element[1].split('/')[:-1]
-      if element[0] == 'd':
-        relative_path = relative_path[:-1]
-      relative_path = '/'.join(relative_path)
-      system("mv " + temp_folder_path + '/' + element[1] + ' ' + home_dir + '/' + relative_path + '/')
-      print "    Restored : " + element[1]
+    # Skip the complete group or try to restore existing items
+    if items_number == 0:
+      output("    Skipped : no iems found")
+    else:
+      for item in config_files[group]:
+        source_path = temp_folder_path + item[1]
+        # Be sure the item exist in the archive
+        if not os.path.exists(source_path):
+          output("    Skipped (item not found) : " + source_path)
+        else:
+          # Erase the destination item to prevent conflicts
+          item_full_path = home_dir + '/' + item[1]
+          system("rm -rf " + item_full_path)
+          output("    Removed : " + item_full_path)
+          # Copy all files of the item to the home dir
+
+          relative_path = item[1].split('/')[:-1]
+          if item[0] == 'd':
+            relative_path = relative_path[:-1]
+          relative_path = '/'.join(relative_path)
+          if len(relative_path) > 0:
+            relative_path += '/'
+          destination_folder = home_dir + '/' + relative_path
+          # Before copying, make sure that intermediate folders exists
+          makeFolderReal(destination_folder)
+          system("mv " + source_path + ' ' + destination_folder)
+          output("    Restored : " + item_full_path)
 
   # Remove temp files
   remove_temp_folder(temp_folder_path)
@@ -169,13 +268,13 @@ if __name__ == "__main__":
 
   # Get all parameters
   try:
-    opts, args = getopt.getopt(sys.argv[1:], "hsr:", ["help", "save", "nodate", "nouser", "restore="])
+    opts, args = getopt.getopt(sys.argv[1:], "hscr:", ["help", "save", "complete", "silent", "nodate", "nouser", "restore="])
   except getopt.GetoptError:
     # print help information and exit:
     usage()
     sys.exit(2)
 
-  # Set the output file
+  # Get all user's options and define internal variables
   restore_file = None
   save_action = False
   use_date = True
@@ -186,11 +285,15 @@ if __name__ == "__main__":
       sys.exit()
     if o in ("-s", "--save"):
       save_action = True
+    if o in ("-c", "--complete"):
+      complete_group_required = True
     if o in ("-r", "--restore"):
       restore_file = a
-    if o in ("--nodate"):
+    if o == "--silent":
+      silent = True
+    if o == "--nodate":
       use_date = False
-    if o in ("--nouser"):
+    if o == "--nouser":
       use_user = False
 
   # Check args number
@@ -199,21 +302,22 @@ if __name__ == "__main__":
     sys.exit(2)
 
   # Format urls
-  for item in config_files.keys():
-    for element in config_files[item]:
-      if element[0] == 'd' and element[1][-1] != '/':
-        old_tuple = config_files[item][config_files[item].index(element)]
+  for group in config_files.keys():
+    for item in config_files[group]:
+      if item[0] == 'd' and item[1][-1] != '/':
+        old_tuple = config_files[group][config_files[group].index(item)]
         new_tuple = (old_tuple[0], old_tuple[1] + '/')
-        config_files[item].remove(old_tuple)
-        config_files[item].append(new_tuple)
-      elif element[0] == 'f' and element[1][-1] == '/':
-        old_tuple = config_files[item][config_files[item].index(element)]
+        config_files[group].remove(old_tuple)
+        config_files[group].append(new_tuple)
+      elif item[0] == 'f' and item[1][-1] == '/':
+        old_tuple = config_files[group][config_files[group].index(item)]
         new_tuple = (old_tuple[0], old_tuple[1][:-1])
-        config_files[item].remove(old_tuple)
-        config_files[item].append(new_tuple)
+        config_files[group].remove(old_tuple)
+        config_files[group].append(new_tuple)
 
   # Do the right action
   if save_action == True:
     save_config(use_date, use_user)
-  elif restore_file != None:
+  elif restore_file not in (None, ''):
+    # TODO: verify here that the restore_file refer to a .tar.bz2 file type (use mime type python mudule ?)
     restore_config(restore_file)
